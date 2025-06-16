fdf75f3c11b55b3fb7bd434796b4bea7df553e9c
Sathsara Rasantha <sathsara@kayatech.com>
2025-06-16 22:23:58 +0530

    added BE folder

--- backend/restaurant_agent.py
+++ backend/restaurant_agent.py
@@ -21,10 +21,10 @@ logger.setLevel(logging.INFO)
 load_dotenv()
 
 voices = {
-    "greeter": "794f9389-aac1-45b6-b726-9d9369183238",
-    "reservation": "156fb8d2-335b-4950-9cb3-a2d33befec77",
-    "takeaway": "6f84f4b8-58a2-430c-8c79-688dad597532",
-    "checkout": "39b376fc-488e-4d0c-8b37-e00b72059fdd",
+    "greeter": "OYTbf65OHHFELVut7v2H",
+    "reservation": "1SM7GgM6IMuvQlz2BwM3",
+    "takeaway": "scOwDtmlUjD3prqpp97I",
+    "checkout": "OYTbf65OHHFELVut7v2H",
 }
 
 
